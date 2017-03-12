540c8cc4ce43d7998c1a5f97bc154d36074e46b5
Anatoliy A Aksenov <anatoliy@aksenov.org>
2017-03-12 22:21:29 +0300

    First commit

---
+++ _frm_Main.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bpMain = new System.Windows.Forms.PictureBox();
+            this.bOpen = new System.Windows.Forms.Button();
+            this.pFaces = new System.Windows.Forms.FlowLayoutPanel();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bpMain)).BeginInit();
+            this.pFaces.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bpMain
+            // 
+            this.bpMain.Location = new System.Drawing.Point(12, 12);
+            this.bpMain.Name = "bpMain";
+            this.bpMain.Size = new System.Drawing.Size(586, 320);
+            this.bpMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.bpMain.TabIndex = 4;
+            this.bpMain.TabStop = false;
+            // 
+            // bOpen
+            // 
+            this.bOpen.Location = new System.Drawing.Point(12, 338);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(75, 23);
+            this.bOpen.TabIndex = 5;
+            this.bOpen.Text = "Open";
+            this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bStart_MouseClick);
+            // 
+            // pFaces
+            // 
+            this.pFaces.AutoScroll = true;
+            this.pFaces.Controls.Add(this.pb1);
+            this.pFaces.Location = new System.Drawing.Point(602, 12);
+            this.pFaces.Name = "pFaces";
+            this.pFaces.Size = new System.Drawing.Size(198, 320);
+            this.pFaces.TabIndex = 6;
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(3, 3);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(183, 110);
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 377);
+            this.Controls.Add(this.pFaces);
+            this.Controls.Add(this.bOpen);
+            this.Controls.Add(this.bpMain);
+            this.Name = "frm_Main";
+            this.Text = "Face detection";
+            ((System.ComponentModel.ISupportInitialize)(this.bpMain)).EndInit();
+            this.pFaces.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bpMain;
+        private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.FlowLayoutPanel pFaces;
+        private System.Windows.Forms.PictureBox pb1;
+    }
+}
+
---
+++ _frm_Main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace WindowsFormsApplication1
+{    
+    public partial class frm_Main : Form
+    {
+        public frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void bStart_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            //CvCapture camera = new CvCapture("C:\\rosbank\\facedetect\\test\\media\\test.avi");
+            var capture = new VideoCapture("C:\\rosbank\\facedetect\\test\\media\\test.avi");
+            //capture.Set(CaptureProperty.FrameWidth, 320);
+            //capture.Set(CaptureProperty.FrameHeight, 240);
+            
+            int sleepTime = (int)Math.Round(1000 / capture.Fps);
+
+            Mat image = new Mat();
+            Rect[] faces = null;
+            int i = 0;
+            while (true)
+            {
+                i++;
+                capture.Read(image); 
+                if (image.Empty())
+                    break;
+
+
+                Mat small = new Mat();
+                Cv2.Resize(image, small, new OpenCvSharp.Size(320, 240), 0, 0, InterpolationFlags.Lanczos4);                
+
+                if (i % 5 == 0)
+                {
+                    var cascade = new CascadeClassifier(@"C:\opencv3\opencv\sources\data\haarcascades\haarcascade_frontalface_alt.xml");
+                    faces = cascade.DetectMultiScale(small, 1.08, 2, HaarDetectionType.ScaleImage, new OpenCvSharp.Size(30, 30));
+                    
+                }
+                if (faces != null && faces.Length > 0)
+                {
+                    foreach (Rect face in faces)
+                    {
+                        var center = new OpenCvSharp.Point
+                        {
+                            X = (int)(face.X + face.Width * 0.5),
+                            Y = (int)(face.Y + face.Height * 0.5)
+                        };
+                        var axes = new OpenCvSharp.Size
+                        {
+                            Width = (int)(face.Width * 0.5),
+                            Height = (int)(face.Height * 0.5)
+                        };
+                        
+                        Mat f = new Mat(small, face);
+                        pb1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(f);
+                        pb1.Refresh();
+
+                        Cv2.Ellipse(small, center, axes, 0, 0, 360, new Scalar(255, 0, 255), 2);
+                    }
+                }
+
+                bpMain.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(small);
+                bpMain.Refresh();
+                Cv2.WaitKey(100);
+            }    
+        }
+    }
+}
